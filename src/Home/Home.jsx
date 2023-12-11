3c188999f5e3755c463bbb493a5afdcd48bb3390
mahfuzar175 <mahfuzar175@gmail.com>
2023-12-11 15:23:27 +0600

    use react particles in home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,10 +3,89 @@ import Banner from "./Banner";
 import Contact from "./Contact";
 import Projects from "./Projects";
 import Skill from "./Skill";
+import Particles from "react-tsparticles";
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim";
 
 const Home = () => {
+    const particlesInit = useCallback(async (engine) => {
+        await loadSlim(engine);
+      }, []);
+    
+      const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+      }, []);
     return (
         <div>
+            <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 400,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 1,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.9,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#0d47a1",
+            },
+            links: {
+              color: "#0d47a1",
+              distance: 150,
+              enable: true,
+              opacity: 1,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 700,
+              },
+              value: 12,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 2, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
             <Banner></Banner>
             <About></About>
             <Skill></Skill>
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,9 +2,12 @@ import Navbar from '../Home/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Home/Footer';
 
+
+
 const Main = () => {
-    return (
-        <div className="flex flex-col min-h-screen">
+
+    return (          
+            <div className="flex flex-col min-h-screen">
             <Navbar></Navbar>
             <div className="flex-1">
             <Outlet></Outlet>
